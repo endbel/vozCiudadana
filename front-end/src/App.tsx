63bf20ba7cbe6143d5cee3b471aec41dc53819c0
DDarioBenitez <reedq2.alt@gmail.com>
2025-10-16 20:05:16 -0300

    pre-merge

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,22 @@
 import "./index.css";
 import Sidebar from "./components/sidebar/Sidebar";
 import Map from "./components/map/Map";
+import { useEffect, useState } from "react";
+import { calculateAge } from "./lib/calculateAge";
 // import StepCards from "./components/steps"
 
 function App() {
+  const [birthDate, setBirthDate] = useState<string | null>(null);
+  const [age, setAge] = useState<number | null>(null);
+
+  useEffect(() => {
+    const storedBirthDate = localStorage.getItem("userBirthDate");
+    if (storedBirthDate) {
+      setBirthDate(storedBirthDate);
+      setAge(calculateAge(storedBirthDate));
+    }
+  }, [birthDate]);
+
   return (
     <div className="w-screen h-screen flex relative">
       {/* Sidebar fijo a la izquierda */}
